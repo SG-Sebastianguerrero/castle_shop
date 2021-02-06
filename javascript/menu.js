5159b6637d2017ae17f8d4a67efe2153e1fafcee
SG-Sebastianguerrero <64206266+SG-Sebastianguerrero@users.noreply.github.com>
2021-02-05 21:45:47 -0500

    Media Queries responsive
    with the media query will search in javascript if the screen is mobile or desktop and based in that will change the hero image.

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,7 +1,33 @@
-$(document).ready(function(){
-  $('.header_barText').hide();
-  $('#btn-menu').click(function(){
-  $('.menu').toggleClass('close');
-  $('.header_barText').slideToggle();
-  });
+//
+// $('.header_barText').show();
+// var mediaqueryList = window.matchMedia("(min-width: 768px)");
+//
+// if(mediaqueryList.matches) {
+//   $(document).ready( function(){
+//   $('.header_barText').show();
+//   });
+//   }
+//
+// if(!mediaqueryList.matches){
+//  $(document).ready( function(){
+//   $('.header_barText').hide();
+//   $('#btn-menu').click(function(){
+//   $('.menu').toggleClass('close');
+//   $('.header_barText').slideToggle();
+//   });
+// });
+// }
+
+
+var mediaqueryList = window.matchMedia("(min-width: 768px)");
+mediaqueryList.addListener( function(EventoMediaQueryList) {
+ if(EventoMediaQueryList.matches) {
+   $('.header_barText').show();
+ }
+ if(!EventoMediaQueryList.matches){
+ $('.header_barText').hide();
+ $('#btn-menu').click(function(){
+ $('.menu').toggleClass('close');
+ $('.header_barText').slideToggle();
+ });}
 });
