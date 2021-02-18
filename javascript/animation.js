f8638523c6c76e0b7665565ca43a591d7572a181
SG-Sebastianguerrero <64206266+SG-Sebastianguerrero@users.noreply.github.com>
2021-02-18 18:59:00 -0500

    animation and responsive hero
    Add a responsive hero in desktop and in mobile version and fix the class errors with the labels ul and li

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,4 +1,4 @@
-  function animation_close(){
+  function animation_close() {
     $('.animation').hide();
   }
-    window.setTimeout(animation_close, 5000);
+  window.setTimeout(animation_close, 5000);
